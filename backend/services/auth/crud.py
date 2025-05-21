c5662a08c57055697053e52278013412f6749187
theaihopgg <theaihopgg@gmail.com>
2025-05-21 23:04:46 +0700

    feat: added authenticate_user

--- backend/services/auth/crud.py
+++ backend/services/auth/crud.py
@@ -1,17 +1,21 @@
 from datetime import datetime, timedelta, timezone
 from typing import Any
+import bcrypt
 from jwt import (
     JWT,
     jwk_from_pem,
     exceptions,
 )
 from jwt.utils import get_int_from_datetime
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from ...core.configuration import (
     AUTH_PRIVATE_KEY_PATH,
     AUTH_JWT_ALGORITHM,
     AUTH_TOKEN_LIFETIME,
 )
+from ..users.crud import get_user_by_email
+from ...core.models import User
 
 jwt = JWT()
 
@@ -48,7 +52,22 @@ def decode_auth_token(
         return None
 
 
+def authenticate_user(
+    user: User,
+    user_email: str,
+    user_password: str,
+) -> bool:
+    return (
+        bcrypt.checkpw(
+            password=user_password.encode(),
+            hashed_password=user.password_hash,
+        )
+        and user.email == user_email
+    )
+
+
 __all__ = (
     "encode_auth_token",
     "decode_auth_token",
+    "authenticate_user",
 )
